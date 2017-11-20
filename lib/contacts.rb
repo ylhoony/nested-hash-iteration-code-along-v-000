f77373249b4c96b7b3f9a684dd06e2a7fe72bdcc
Hoon Lee <yl.hoony@gmail.com>
2017-11-20 03:08:36 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute|
+      binding.pry
+    end
+  end
 end
